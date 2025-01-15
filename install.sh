1657cfbfeb3e43bfb36c1a1153310dc31b2eb0a5
«JulienJcbs» <julien.j.1999@hotmail.com>
2025-01-15 14:32:12 +0100

    Correction des commandes d'installation pour utiliser pip directement et suppression de sudo

--- install.sh
+++ install.sh
@@ -10,10 +10,10 @@ python3 -m venv env
 
 source env/bin/activate
 
-sudo /home/pi/parfum_2/env/bin/python pip install RPi.GPIO
-sudo /home/pi/parfum_2/env/bin/python pip install board
-sudo /home/pi/parfum_2/env/bin/python pip install neopixel
-sudo /home/pi/parfum_2/env/bin/python pip install adafruit-blinka
-sudo /home/pi/parfum_2/env/bin/python pip install adafruit-circuitpython-neopixel
+/home/pi/parfum_2/env/bin/pip install RPi.GPIO
+/home/pi/parfum_2/env/bin/pip install board
+/home/pi/parfum_2/env/bin/pip install neopixel
+/home/pi/parfum_2/env/bin/pip install adafruit-blinka
+/home/pi/parfum_2/env/bin/pip install adafruit-circuitpython-neopixel
 
 echo 'Finish'
