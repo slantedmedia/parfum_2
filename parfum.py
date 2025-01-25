b989796cce59fa2b9cd46fbc206bfddd24917c0b
Julien Jacobs <54290972+JulienJcbs@users.noreply.github.com>
2025-01-25 15:38:27 +0100

    Update parfum.py

--- parfum.py
+++ parfum.py
@@ -21,7 +21,7 @@ last_event_time = 0  # Stocke l'horodatage du dernier événement
 event_cooldown = 2  # Temps minimum (en secondes) entre deux impulsions
 event_handled = set()  # Ensemble pour mémoriser les événements traités
 
-GPIO.cleanup(4)
+GPIO.cleanup()
 print("GPIO clean avec succès.")
 
 # Exécuter la commande adb logcat -c
